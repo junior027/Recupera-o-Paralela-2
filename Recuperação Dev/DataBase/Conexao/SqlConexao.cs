d1d88ee74df47e9c28610ae6eb0abd5f65502d77
Carlito Junior <carlito.junior@es.estudante.senai.br>
2023-11-23 20:21:44 -0300

    feat: Codigos para conexão ao banco, usuariorepositorio, endidades, e na comunicação com a tela de usuario;

--- Recuperação Dev/DataBase/Conexao/SqlConexao.cs
+++ Recuperação Dev/DataBase/Conexao/SqlConexao.cs
@@ -8,5 +8,13 @@ namespace DataBase.Conexao
 {
     internal class SqlConexao
     {
+        /// <summary>
+        /// Metodo que <c>StrConexao()</c> retorna a string de conexão sql
+        /// </summary>
+        /// <returns>string: ConnectionString</returns>
+        internal static string StrConexao()
+        {
+            return @"Data Source=.\SQLEXPRESS;Initial Catalog=AdvAbc;User ID=sa;Password=sql2022;Trusted_Connection=False; TrustServerCertificate=True;";
+        }
     }
 }
--- Recuperação Dev/Negocio/Negocio/Entidades/EntidadeUsuario.cs
+++ Recuperação Dev/Negocio/Negocio/Entidades/EntidadeUsuario.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Negocio.Entidades
+﻿namespace Negocio.Entidades
 {
-    internal class EntidadeUsuario
+    public class EntidadeUsuario
     {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        public DateTime Contato { get; set; }
+        public string Cargo { get; set; }
+        public string Empresa { get; set; }
+        public EntidadeUsuario(string nome, string telefone, string email, string endereco)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Email = email;
+            Endereco = endereco;
+        }
     }
 }
